564a7db1f0bcc6a54fbca24087d5e6271af7250d
João Godoi <joao.godoi@skyone.solutions>
2024-11-20 23:32:21 -0300

    Improved logo and home page

---
+++ apps/frontend/src/app/(pages)/layout.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import Page from "@/components/shared/templates/Page";
+
+export default function Layout(props: any) {
+  return <Page>{props.children}</Page>;
+}
+ 
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,23 @@
-import Page from "@/components/shared/templates/Page";
+import Logo from "@/components/shared/templates/Logo";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Page>
-      <div>Inicio</div>
-    </Page>
+    <div
+      className="
+    h-screen flex flex-col justify-center items-center gap-10
+    bg-[url('/background-home.svg')] bg-cover
+    "
+    >
+      <div className="flex flex-col items-center gap-4">
+        <Logo />
+        <p className="text-zinc-500 font-light w-96 leading-6 text-center">
+          Crie e gerencie seus eventos de forma simples e eficiente.
+        </p>
+      </div>
+      <Link href="/event" className="button blue text-lg uppercase">
+        Criar novo evento
+      </Link>
+    </div>
   );
 }
